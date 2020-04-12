fdcc26c5a3122aee6782341ec8765ad8f9b18076
Adarsh Nanwani <adarsh.nanwani@gmail.com>
2020-04-12 18:27:23 +0530

    Add BookType graphQL schema.

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const graphqlHTTP = require('express-graphql');
 require('dotenv').config({
   path: './.env',
 });
+const schema = require('./schema/schema');
 
 const app = express();
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema,
+  })
+);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
