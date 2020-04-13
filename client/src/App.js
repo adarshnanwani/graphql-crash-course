abb3fc97a5e77ced8950073e520ddee9356cbb11
Adarsh Nanwani <adarsh.nanwani@gmail.com>
2020-04-13 15:26:14 +0530

    Add apollo client on frontend.

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
 import './App.css';
 import BookList from './components/BookList';
 
+const client = new ApolloClient({
+  uri: '/graphql',
+});
+
 function App() {
   return (
-    <div className='App'>
-      <BookList />
-    </div>
+    <ApolloProvider client={client}>
+      <div className='App'>
+        <BookList />
+      </div>
+    </ApolloProvider>
   );
 }
 
