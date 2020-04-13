3fadcd2b59be0dd64e790636e6f14bd381af41a1
Adarsh Nanwani <adarsh.nanwani@gmail.com>
2020-04-13 15:11:32 +0530

    Add booklist component.

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import './App.css';
+import BookList from './components/BookList';
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <BookList />
+    </div>
+  );
 }
 
 export default App;
---
+++ client/src/components/BookList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const BookList = (props) => {
+  return (
+    <div>
+      <ul>
+        <li>Book </li>
+      </ul>
+    </div>
+  );
+};
+
+BookList.propTypes = {};
+
+export default BookList;
