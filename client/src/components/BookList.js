0885e933d73b2b6855693f007a604aac43b833fc
Adarsh Nanwani <adarsh.nanwani@gmail.com>
2020-04-13 17:06:24 +0530

    Display data in booklist.

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -12,13 +12,19 @@ const getBooksQuery = gql`
   }
 `;
 
-const BookList = (props) => {
-  console.log(props.data.books);
+const BookList = ({ data: { loading, books } }) => {
+  const displayBooks = () => {
+    if (loading) {
+      return <h4>Loading...</h4>;
+    } else {
+      return books && books.map((book) => <li key={book.id}>{book.name}</li>);
+    }
+  };
+
   return (
-    <div>
-      <ul>
-        <li>Book </li>
-      </ul>
+    <div id='book-list'>
+      <h2>Reading List</h2>
+      <ul>{displayBooks()}</ul>
     </div>
   );
 };
