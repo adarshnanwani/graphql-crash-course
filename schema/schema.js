76ab0a51ae06a37d86ab9d387c91edbf290f13f9
Adarsh Nanwani <adarsh.nanwani@gmail.com>
2020-04-13 13:23:05 +0530

    Update the resolver functions to use mongodb queries.

--- schema/schema.js
+++ schema/schema.js
@@ -23,7 +23,7 @@ const {
 // ];
 
 // const authors = [
-//   { name: 'Patrick Rothfuss', age: 44, id: '1' }, //  
+//   { name: 'Patrick Rothfuss', age: 44, id: '1' }, //
 //   { name: 'Brandon Sanderson', age: 42, id: '2' },
 //   { name: 'Terry Prachett', age: 66, id: '3' },
 // ];
@@ -44,6 +44,7 @@ const BookType = new GraphQLObjectType({
       type: AuthorType,
       resolve(parent, args) {
         // return _.find(authors, { id: parent.authorId });
+        return Author.findById(parent.author);
       },
     },
   }),
@@ -65,6 +66,7 @@ const AuthorType = new GraphQLObjectType({
       type: new GraphQLList(BookType),
       resolve(parent, args) {
         // return _.filter(books, { authorId: parent.id });
+        return Book.find({ author: parent.id });
       },
     },
   }),
@@ -79,6 +81,7 @@ const RootQuery = new GraphQLObjectType({
       resolve(parent, args) {
         // code to get data from db / other source
         // return _.find(books, { id: args.id });
+        return Book.findById(args.id);
       },
     },
     author: {
@@ -87,18 +90,21 @@ const RootQuery = new GraphQLObjectType({
       resolve(parent, args) {
         // code to get data from db / other source
         // return _.find(authors, { id: args.id });
+        return Author.findById(args.id);
       },
     },
     books: {
       type: new GraphQLList(BookType),
       resolve(parent, args) {
         // return books;
+        return Book.find();
       },
     },
     authors: {
       type: new GraphQLList(AuthorType),
       resolve(parent, args) {
         // return authors;
+        return Author.find();
       },
     },
   },
